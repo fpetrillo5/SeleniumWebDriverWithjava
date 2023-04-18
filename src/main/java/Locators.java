2abb2f88b7b0a260aea4f42bc3cc87be1429d72f
Frank Petrillo <frankpetrillo5@gmail.com>
2023-04-18 16:20:03 -0400

    added remote allow origins fix to selenium

--- src/main/java/Locators.java
+++ src/main/java/Locators.java
@@ -1,15 +1,31 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
 
 public class Locators {
     public static void main(String[] args){
-        WebDriver driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
         driver.findElement(By.id("inputUsername")).sendKeys("rahul");
         driver.findElement(By.name("inputPassword")).sendKeys("hello23");
         driver.findElement(By.className("submit")).click();
-       driver.findElement(By.className("error")).getText();
+        System.out.println( driver.findElement(By.className("error")).getText());
+        driver.findElement(By.linkText("Forgot your password?")).click();
+        System.out.println(driver.getTitle());
+        driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("Paco");
+        driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("paco@wells.com");
+        //driver.findElement(By.cssSelector("input[@placeholder='Email']")).clear();
+        driver.findElement(By.xpath("//form/input[3]")).sendKeys("610-456-7890");
+        driver.findElement(By.className(".reset-pwd-btn")).click();
+        System.out.println(driver.findElement(By.className(".infoMsg")).getText());
+
 
         //driver.close();
     }
--- src/main/java/SeleniumIntro.java
+++ src/main/java/SeleniumIntro.java
@@ -1,11 +1,16 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class SeleniumIntro {
 
     public static void main(String[] args){
 
-        WebDriver driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
 
         System.out.println(driver.getTitle());
